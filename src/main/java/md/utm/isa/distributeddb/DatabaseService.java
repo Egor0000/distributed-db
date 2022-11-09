18e2f908e2b4a2eaf3f83d393fee0904ad6acdee
Egor Babcinetchi <egor.babcinetchi@isa.utm.md>
2022-11-09 11:29:30 +0200

    add checkpoint 1

--- src/main/java/md/utm/isa/distributeddb/DatabaseService.java
+++ src/main/java/md/utm/isa/distributeddb/DatabaseService.java
@@ -4,4 +4,6 @@ public interface DatabaseService {
     String set(Entity entity) throws Exception;
     Entity read(String key);
     void delete(String key);
+
+    String saveToDb(Entity entity);
 }
--- src/main/java/md/utm/isa/distributeddb/DatabaseServiceImpl.java
+++ src/main/java/md/utm/isa/distributeddb/DatabaseServiceImpl.java
@@ -1,20 +1,47 @@
 package md.utm.isa.distributeddb;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 @Service
-@RequiredArgsConstructor
 public class DatabaseServiceImpl implements DatabaseService {
     private final TcpClient tcpClient;
     private final ConcurrentMap<String, String> db = new ConcurrentHashMap<>();
+    private final String servers;
+    private final String leader;
+    private final String id;
+    private String name;
+
+    public DatabaseServiceImpl(TcpClient tcpClient, @Value("${serverss}") String servers,
+                               @Value("${name}") String name,
+                               @Value("${leader}") String leader,
+                               @Value("${data.id}") String id) {
+        this.tcpClient = tcpClient;
+        this.name = name;
+        this.leader = leader;
+        this.id = id;
+        this.servers = servers;
+    }
+
     @Override
     public String set(Entity entity) throws Exception {
         try {
-            tcpClient.send("localhost", 4445, entity);
+            if (id.equals(leader)) {
+                for (String server : servers.split(",")) {
+                    String[] address = server.split(":");
+                    if (!address[0].equals(name)) {
+                        try {
+                            tcpClient.send(address[0], Integer.parseInt(address[1]), entity);
+                        } catch (Exception ex) {
+
+                        }
+                    }
+                }
+            }
         } catch (Exception ex) {
             throw new RuntimeException(ex);
         }
@@ -29,5 +56,10 @@ public class DatabaseServiceImpl implements DatabaseService {
     @Override
     public void delete(String key) {
         db.remove(key);
+    }
+
+    @Override
+    public String saveToDb(Entity entity) {
+        return db.put(entity.getKey(), entity.getValue());
     }
 }
